21bc3451fe841261372782756da1b868d6f37bc4
s-matyukevich <s.matyukevich@gmail.com>
2016-07-09 03:49:03 +0300

    values escaped

--- lib/dpl/provider/cloud_foundry.rb
+++ lib/dpl/provider/cloud_foundry.rb
@@ -11,7 +11,7 @@ module DPL
       def check_auth
         initial_go_tools_install
         context.shell "./cf api #{option(:api)} #{'--skip-ssl-validation' if options[:skip_ssl_validation]}"
-        context.shell "./cf set-token -a #{options[:access_tokens]} -r #{options[:refresh_tokens]} -r #{options[:oauth_clients]} -p #{options[:oauth_client_passwords]}"
+        context.shell "./cf set-token -a '#{options[:access_tokens]}' -r '#{options[:refresh_tokens]}' -r '#{options[:oauth_clients]}' -p '#{options[:oauth_client_passwords]}'"
         context.shell "CF_TRACE=true ./cf target -o #{option(:organization)} -s #{option(:space)}"
       end
 
